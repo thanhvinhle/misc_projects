56896badbcae51a1b299c592174a4d094083a376
Thanh Le <thanhle@Thanhs-MacBook-Pro.local>
2016-12-01 22:50:13 -0800

    Using Python template to change the 'index' view

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
@@ -15,3 +19,5 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
