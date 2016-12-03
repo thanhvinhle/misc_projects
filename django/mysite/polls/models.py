d38f82a7a592cc14bd9f18b984f16b443266df78
Thanh Le <thanhle@Thanhs-MacBook-Pro.local>
2016-12-02 16:58:59 -0800

    Part 5 - Adding a small testcase

--- django/mysite/polls/models.py
+++ django/mysite/polls/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 class Question(models.Model):
@@ -8,6 +9,9 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
       return self.question_text
+    def was_published_recently(self):
+      now = timezone.now()
+      return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
