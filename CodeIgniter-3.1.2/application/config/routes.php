1caf53dc93bf340cc4627b75e3c4df6b65a98c1a
Thanh Le <thanhle@Thanhs-MacBook-Pro.local>
2016-12-26 12:07:52 -0800

    News section

--- CodeIgniter-3.1.2/application/config/routes.php
+++ CodeIgniter-3.1.2/application/config/routes.php
@@ -55,5 +55,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$route['404_override'] = '';
 //$route['translate_uri_dashes'] = FALSE;
 
+//$route['default_controller'] = 'pages/view';
+//$route['(:any)'] = 'pages/view/$1';   # Matches any request and passes the parameter to the view() method of the Pages class
+
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
+$route['(:any)'] = 'pages/view/$1';
 $route['default_controller'] = 'pages/view';
-$route['(:any)'] = 'pages/view/$1';   # Matches any request and passes the parameter to the view() method of the Pages class
